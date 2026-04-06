eee23bcadf2753efa514c19158f354419a59d7cc
Amit Schnitzer <amit.schnitzer@gmail.com>
2026-04-06 13:06:52 -0400

    chore: update backend to use aschnitzer-terraform-state-files bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-state-files"
+    bucket = "aschnitzer-terraform-state-files"
     key    = "drift-detection-demo-yor/terraform.tfstate"
     region = "us-east-1"
   }
